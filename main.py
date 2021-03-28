f1dda6e1f9035c6cabc842056c49f4dba99dec46
miloujb <emilybailey@Emilys-MacBook-Air.local>
2021-03-28 12:11:17 +0100

    time defined

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ from tkinter.ttk import *
 
 from time import strftime
 
+root = Tk()
+root.title('Clock')
+
+def time():
+    string = strftime('%H:%M:%S %p')
+    lbl.config(text = string)
+    lbl.after(1000, time)
+
